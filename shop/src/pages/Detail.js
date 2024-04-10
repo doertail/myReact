ae5330135d13482c98fe7529de1aab431b87784e
오지훈 <doer.tail@gmail.com>
2024-04-10 22:17:05 +0900

    install styled-components

--- shop/src/pages/Detail.js
+++ shop/src/pages/Detail.js
@@ -3,6 +3,7 @@ function Detail(props) {
   let {id} = useParams();
   let newId = parseInt(id,10) + 1;
   let url = "https://codingapple1.github.io/shop/shoes" + newId + ".jpg";
+  const target = props.shoes.find(obj => obj.id == id);
 
     return (
       <div className="container">
@@ -11,9 +12,9 @@ function Detail(props) {
             <img src={url} width="100%" />
           </div>
           <div className="col-md-6">
-            <h4 className="pt-5">{props.shoes[id].title}</h4>
-            <p>{props.shoes[id].content}</p>
-            <p>{props.shoes[id].price}</p>
+            <h4 className="pt-5">{target.title}</h4>
+            <p>{target.content}</p>
+            <p>{target.price}</p>
             <button className="btn btn-danger">주문하기</button> 
           </div>
         </div>
